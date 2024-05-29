e0be5817917694bc1ecae090b1c75df3f3eb5d2f
Lamr <Lamr>
2024-05-29 23:48:00 +0300

    Init

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 def add_numbers(a, b):
     return a + b
 
+
 def test_add_numbers():
     assert add_numbers(3, 4) == 7
+    assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
+
 
 test_add_numbers()
